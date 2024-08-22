92e82608299263670a8aef81cb7ca1d40e6747ef
Adam <astevens9234@outlook.com>
2024-08-22 11:35:39 -0700

    added React and some scaffolding

--- API/Program.cs
+++ API/Program.cs
@@ -1,49 +1,66 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Think of services as things we can use inside our code.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
+internal class Program
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        // Add services to the container.
+        // Think of services as things we can use inside our code.
 
-// Configure the HTTP request pipeline. Any middlewear goes here.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+        builder.Services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .WithOrigins("http://localhost:3000");
+            });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline. Any middlewear goes here.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        //app.UseHttpsRedirection();
+
+        app.UseCors("CorsPolicy");
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        // Start up the DB
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedData(context);
+        }
+        catch (Exception err)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(err, "Error During Migration.");
+        }
+
+        app.Run();
+    }
 }
-
-//app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-// Start up the DB
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
-}
-catch (Exception err)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(err, "Error During Migration.");
-}
-
-app.Run();
